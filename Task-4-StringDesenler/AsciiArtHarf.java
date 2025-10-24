dcd92b7273be2e4d8570a29ad0933290f6fb0863
ulkutuanna <ulkusevgi123654@gmail.com>
2025-10-24 13:30:13 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,9 @@
+public class AsciiArtHarf {
+    public static void main(String[] args) {
+        System.out.println("TTTTTTTTTT");
+        System.out.println("    TT    ");
+        System.out.println("    TT    ");
+        System.out.println("    TT    ");
+        System.out.println("    TT    ");
+    }
+}
