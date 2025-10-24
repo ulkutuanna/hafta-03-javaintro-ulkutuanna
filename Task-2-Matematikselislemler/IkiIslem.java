d54c4e745ec132de587768bed6d23ca32c520e52
ulkutuanna <ulkusevgi123654@gmail.com>
2025-10-24 13:09:21 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,8 @@
+public class IkiIslem {
+    public static void main(String[] args) {
+        double pay = 9.5 * 4.5 - 2.5 * 3;
+        double payda = 45.5 - 3.5;
+        double sonuc = pay / payda;
+        System.out.printf("Sonuc = %.4f%n", sonuc);
+    }
+}
