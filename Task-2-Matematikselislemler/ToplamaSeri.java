4a8fcb252734d2b8180bb5d4a215834477db13bc
ulkutuanna <ulkusevgi123654@gmail.com>
2025-10-24 12:55:21 +0300

    Add ToplamaSerisi class for summing numbers

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,7 @@
+public class ToplamaSerisi {
+    public static void main(String[] args) {
+        int sonuc = 1+2+3+4+5+6+7+8+9;
+        System.out.println("Toplama Serisi Sonucu: " + sonuc);
+    }
+
+}
