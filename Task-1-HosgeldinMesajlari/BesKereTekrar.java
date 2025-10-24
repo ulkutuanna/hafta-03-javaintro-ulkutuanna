9d66eb71ab37b5a6fdb4cc3da508e8a3499a2961
ulkutuanna <ulkusevgi123654@gmail.com>
2025-10-24 12:47:01 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,12 @@
+public class BesTekrar {
+    public static void main(String[] args) {
+         
+            System.out.println("Java'ya Hoşgeldiniz");
+            System.out.println("Java'ya Hoşgeldiniz");
+            System.out.println("Java'ya Hoşgeldiniz");
+            System.out.println("Java'ya Hoşgeldiniz");
+            System.out.println("Java'ya Hoşgeldiniz");
+   
+    }
+
+}
